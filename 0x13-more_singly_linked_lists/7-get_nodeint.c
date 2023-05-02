7ba6043430a7b61be37b04ad9d0c2d372538822e
enampeters <enampeters@gmail.com>
2023-05-02 13:24:46 +0000

    More singly linked list project

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index -  locates a given node of a linked list
+ * @head: pointer to the head of thelistint_t list
+ * @index: index of the node to locate starting from 0
+ *
+ * Return: NULL if node does not exist else the located node.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int node;
+
+	for (node = 0; node < index; node++)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
+}
