73973f9557cc00a439a54d2cc110caf0e96ab50b
enampeters <enampeters@gmail.com>
2023-06-22 17:17:46 +0000

    Varaidic functions project

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,30 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+/**
+ * File: variadic_function.h
+ * Desc: header file containing prototypes of all functions used
+ *	in this project
+ */
+
+#include <stdarg.h>
+
+/**
+ * struct printer - A new struct type defining a printer
+ * @symbol: a symbol representing a data type
+ * @print: a function pointer to a function that prints
+ *		a data type corresponding to symbol
+ */
+
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
